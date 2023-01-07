78768e4a87554e85f0a04f98abc6cd0623d1f261
ShinJaehun <gunsmoke.shin@gmail.com>
2023-01-07 12:09:57 +0900

    create main layout

--- app/src/main/java/com/shinjaehun/mymemory/MainActivity.kt
+++ app/src/main/java/com/shinjaehun/mymemory/MainActivity.kt
@@ -2,10 +2,22 @@ package com.shinjaehun.mymemory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvBoard: RecyclerView
+    private lateinit var tvNumMoves: TextView
+    private lateinit var tvNumPairs: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvBoard = findViewById(R.id.rvBoard)
+        tvNumMoves = findViewById(R.id.tvNumMoves)
+        tvNumPairs = findViewById(R.id.tvNumPairs)
     }
 }
