f27affab098ee60f448def14d59e5b255ca8bb1f
ShinJaehun <gunsmoke.shin@gmail.com>
2023-01-08 13:26:04 +0900

    create the memory cards and game classes

--- app/src/main/java/com/shinjaehun/mymemory/MainActivity.kt
+++ app/src/main/java/com/shinjaehun/mymemory/MainActivity.kt
@@ -2,13 +2,18 @@ package com.shinjaehun.mymemory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.shinjaehun.mymemory.models.BoardSize
+import com.shinjaehun.mymemory.models.MemoryCard
+import com.shinjaehun.mymemory.models.MemoryGame
 import com.shinjaehun.mymemory.utils.DEFAULT_ICONS
 
 class MainActivity : AppCompatActivity() {
+
+    companion object val TAG = "MainActivity"
 
     private lateinit var rvBoard: RecyclerView
     private lateinit var tvNumMoves: TextView
@@ -24,10 +29,22 @@ class MainActivity : AppCompatActivity() {
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
 
-        val chosenImages = DEFAULT_ICONS.shuffled().take(boardSize.getNumPairs()) // 카드 한벌 shuffled, pairs만큼
-        val randomizedImages = (chosenImages + chosenImages).shuffled() // 카드 두 벌 shuffled
+        val memoryGame = MemoryGame(boardSize)
 
-        rvBoard.adapter = MemoryBoardAdapter(this, boardSize, randomizedImages)
+//        val chosenImages = DEFAULT_ICONS.shuffled().take(boardSize.getNumPairs()) // 카드 한벌 shuffled, pairs만큼
+//        val randomizedImages = (chosenImages + chosenImages).shuffled() // 카드 두 벌 shuffled
+//        // val memoryCards = randomizedImages.map { MemoryCard(it, false, false) } // 같은 거...(default value)
+//        val memoryCards = randomizedImages.map { MemoryCard(it) } // 이런 식으로 Image lists를 class list로 map 가능!!!
+
+//        rvBoard.adapter = MemoryBoardAdapter(this, boardSize, randomizedImages)
+//        rvBoard.adapter = MemoryBoardAdapter(this, boardSize, memoryCards)
+//        rvBoard.adapter = MemoryBoardAdapter(this, boardSize, memoryGame.cards)
+        rvBoard.adapter = MemoryBoardAdapter(this, boardSize, memoryGame.cards, object: MemoryBoardAdapter.CardClickListener{
+            override fun onCardClicked(position: Int) {
+                Log.i(TAG, "Card clicked $position")
+            }
+        })
+
         rvBoard.setHasFixedSize(true)
         rvBoard.layoutManager = GridLayoutManager(this, boardSize.getWidth())
     }
--- app/src/main/java/com/shinjaehun/mymemory/MemoryBoardAdapter.kt
+++ app/src/main/java/com/shinjaehun/mymemory/MemoryBoardAdapter.kt
@@ -10,18 +10,27 @@ import android.widget.ImageButton
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.shinjaehun.mymemory.models.BoardSize
+import com.shinjaehun.mymemory.models.MemoryCard
 import kotlin.math.min
 
 class MemoryBoardAdapter(
     private val context: Context,
     private val boardSize: BoardSize,
-    private val cardImages: List<Int>
+//    private val cardImages: List<Int>
+    private val cards: List<MemoryCard>,
+    private val cardClickListener: CardClickListener
 ) :
     RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
 
     companion object {
         private const val MARGIN_SIZE = 10
         private const val TAG = "MemoryBoardAdapter"
+    }
+
+    interface CardClickListener {
+        // CardClickListener를 argument로 받는 건 알겠는데 여기서 interface로 선언해야 하는 이유는?
+        // onCardClicked() 구현을 강요하기 위함?
+        fun onCardClicked(position: Int)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -46,9 +55,14 @@ class MemoryBoardAdapter(
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         private val imageButton = itemView.findViewById<ImageButton>(R.id.imageButton)
         fun bind(position: Int) {
-            imageButton.setImageResource(cardImages[position]) // 걍 imageButton 위에 이미지 씌우기
+//            imageButton.setImageResource(cardImages[position]) // 걍 imageButton 위에 이미지 씌우기
+            // imageButton.setImageResource(if (cards[position].isFaceUp) cards[position].identifier else R.drawable.ic_launcher_background) // 그냥 이렇게 해도 될꺼 같은디...
+            val memoryCard = cards[position]
+            imageButton.setImageResource(if (memoryCard.isFaceUp) memoryCard.identifier else R.drawable.ic_launcher_background)
+
             imageButton.setOnClickListener{
                 Log.i(TAG,"Clicked on position $position")
+                cardClickListener.onCardClicked(position)
             }
         }
     }
