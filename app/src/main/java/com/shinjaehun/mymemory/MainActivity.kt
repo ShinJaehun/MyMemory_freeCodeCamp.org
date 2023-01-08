3b7b7c52d83a30e77679fdc157c8cdb52b1c7b63
ShinJaehun <gunsmoke.shin@gmail.com>
2023-01-08 13:05:10 +0900

    add boardsize enum + create icons

--- app/src/main/java/com/shinjaehun/mymemory/MainActivity.kt
+++ app/src/main/java/com/shinjaehun/mymemory/MainActivity.kt
@@ -5,13 +5,16 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import org.w3c.dom.Text
+import com.shinjaehun.mymemory.models.BoardSize
+import com.shinjaehun.mymemory.utils.DEFAULT_ICONS
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var rvBoard: RecyclerView
     private lateinit var tvNumMoves: TextView
     private lateinit var tvNumPairs: TextView
+
+    private var boardSize: BoardSize = BoardSize.EASY
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +24,11 @@ class MainActivity : AppCompatActivity() {
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
 
-        rvBoard.adapter = MemoryBoardAdapter(this, 8)
+        val chosenImages = DEFAULT_ICONS.shuffled().take(boardSize.getNumPairs()) // 카드 한벌 shuffled, pairs만큼
+        val randomizedImages = (chosenImages + chosenImages).shuffled() // 카드 두 벌 shuffled
+
+        rvBoard.adapter = MemoryBoardAdapter(this, boardSize, randomizedImages)
         rvBoard.setHasFixedSize(true)
-        rvBoard.layoutManager = GridLayoutManager(this, 2)
+        rvBoard.layoutManager = GridLayoutManager(this, boardSize.getWidth())
     }
 }
--- app/src/main/java/com/shinjaehun/mymemory/MemoryBoardAdapter.kt
+++ app/src/main/java/com/shinjaehun/mymemory/MemoryBoardAdapter.kt
@@ -9,9 +9,14 @@ import android.view.ViewGroup.MarginLayoutParams
 import android.widget.ImageButton
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.shinjaehun.mymemory.models.BoardSize
 import kotlin.math.min
 
-class MemoryBoardAdapter(private val context: Context, private val numPieces: Int) :
+class MemoryBoardAdapter(
+    private val context: Context,
+    private val boardSize: BoardSize,
+    private val cardImages: List<Int>
+) :
     RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
 
     companion object {
@@ -20,8 +25,8 @@ class MemoryBoardAdapter(private val context: Context, private val numPieces: In
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val cardWidth = parent.width / 2 - (2 * MARGIN_SIZE)
-        val cardHeight = parent.height / 4 - (2 * MARGIN_SIZE)
+        val cardWidth = parent.width / boardSize.getWidth() - (2 * MARGIN_SIZE)
+        val cardHeight = parent.height / boardSize.getHeight() - (2 * MARGIN_SIZE)
         val cardSideLength = min(cardWidth, cardHeight)
         val view = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
         val layoutParams = view.findViewById<CardView>(R.id.cardView).layoutParams as MarginLayoutParams
@@ -36,11 +41,12 @@ class MemoryBoardAdapter(private val context: Context, private val numPieces: In
         holder.bind(position)
     }
 
-    override fun getItemCount() = numPieces
+    override fun getItemCount() = boardSize.numCards
 
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         private val imageButton = itemView.findViewById<ImageButton>(R.id.imageButton)
         fun bind(position: Int) {
+            imageButton.setImageResource(cardImages[position]) // 걍 imageButton 위에 이미지 씌우기
             imageButton.setOnClickListener{
                 Log.i(TAG,"Clicked on position $position")
             }
